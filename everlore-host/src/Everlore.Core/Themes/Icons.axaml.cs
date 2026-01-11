f4c3adc5e0027137f92897afb2da9442a2ccf733
Pedro Pinto <denczh@outlook.com>
2026-01-11 13:17:30 +0100

    Reordering of styles and icons.

---
+++ everlore-host/src/Everlore.Core/Themes/Icons.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Everlore.Core.Themes;
+
+public partial class Icons : ResourceDictionary
+{
+    public static readonly string Home       = "Icon.Home";
+    public static readonly string Settings   = "Icon.Settings";
+    public static readonly string Lock       = "Icon.Lock";
+    public static readonly string Hamburger  = "Icon.Hamburger";
+    public static readonly string ReplyArrow = "Icon.ReplyArrow";
+    
+    public Icons() => InitializeComponent();
+
+    private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+}
