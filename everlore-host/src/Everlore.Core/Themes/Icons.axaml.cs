eda32186cd23a8082b8d5e475ce11b0f1149f85e
Pedro Pinto <denczh@outlook.com>
2026-01-15 18:03:33 +0100

    Plain icons added to module Hero.

--- everlore-host/src/Everlore.Core/Themes/Icons.axaml.cs
+++ everlore-host/src/Everlore.Core/Themes/Icons.axaml.cs
@@ -7,11 +7,14 @@ namespace Everlore.Core.Themes;
 [UsedImplicitly]
 public partial class Icons : ResourceDictionary
 {
-    public static readonly string Home       = "Icon.Home";
     public static readonly string Settings   = "Icon.Settings";
-    public static readonly string Lock       = "Icon.Lock";
     public static readonly string Hamburger  = "Icon.Hamburger";
     public static readonly string ReplyArrow = "Icon.ReplyArrow";
+    public static readonly string Main       = "Icon.Main";
+    public static readonly string Bookmark   = "Icon.Bookmark";
+    public static readonly string Tag        = "Icon.Tag";
+    public static readonly string Delete     = "Icon.Delete";
+    public static readonly string AddItem    = "Icon.AddItem";
     
     public Icons() => InitializeComponent();
 
